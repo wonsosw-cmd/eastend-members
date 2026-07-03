b701712d9bb22aa4fac315b3ec69cab63e148923
wonsosw-cmd <wonsosw@eastend.co.kr>
2026-07-03 17:48:39 +0900

    GAS 웹앱 URL 연결

--- docs/config.js
+++ docs/config.js
@@ -2,5 +2,5 @@
 // Apps Script 웹앱 배포 후 발급되는 URL을 아래에 붙여넣으세요.
 // 예: https://script.google.com/macros/s/AKfycb.../exec
 window.EE_CONFIG = {
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbxPv8TpyUbnLYvs2ZDLbxuoTM9-Z1BiCpjMEr2VmT7UQf31dV-OMlpyHzhPQE7LQB2E/exec",
 };
